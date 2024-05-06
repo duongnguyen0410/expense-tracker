bebe54ef3b32fc3a11b41acd17c860160d08c3de
Nguyen Xuan Duong <20521220@gm.uit.edu.vn>
2024-05-06 15:43:48 +0700

    Update

---
+++ app/(routes)/dashboard/expenses/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Expenses() {
+  return (
+    <div className='p-10'>
+        <h2 className='font-bold text-3xl'>My Expenses</h2>
+    </div>
+  )
+}
+
+export default Expenses
---
+++ app/(routes)/dashboard/upgrade/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Upgrade() {
+  return (
+    <div className='p-10'>
+        <h2 className='font-bold text-3xl mb-5'>Upgrade</h2>
+        <h2>Coming soon...</h2>
+    </div>
+  )
+}
+
+export default Upgrade
